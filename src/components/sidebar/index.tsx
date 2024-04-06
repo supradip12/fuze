20350da6dabc0d6a293a10b72416a47b0e8819e5
Supra <supradipdey1@gmail.com>
2024-04-06 11:56:08 +0530

    update1

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -8,11 +8,11 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-// import { menuOptions } from '@/lib/constant'
+import { menuOptions } from "@/lib/constant";
 import clsx from "clsx";
 import { Separator } from "@/components/ui/separator";
 import { Database, GitBranch, LucideMousePointerClick } from "lucide-react";
-// import { ModeToggle } from '../global/mode-toggle'
+import { ModeToggle } from "../global/mode-toggle";
 
 type Props = {};
 
@@ -25,8 +25,8 @@ const MenuOptions = (props: Props) => {
         <Link className="flex font-bold flex-row " href="/">
           fuzzie.
         </Link>
-        {/* <TooltipProvider> */}
-        {/* {menuOptions.map((menuItem) => (
+        <TooltipProvider>
+          {menuOptions.map((menuItem) => (
             <ul key={menuItem.name}>
               <Tooltip delayDuration={0}>
                 <TooltipTrigger>
@@ -34,9 +34,9 @@ const MenuOptions = (props: Props) => {
                     <Link
                       href={menuItem.href}
                       className={clsx(
-                        'group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer',
+                        "group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer",
                         {
-                          'dark:bg-[#2F006B] bg-[#EEE0FF] ':
+                          "dark:bg-[#2F006B] bg-[#EEE0FF] ":
                             pathName === menuItem.href,
                         }
                       )}
@@ -55,8 +55,8 @@ const MenuOptions = (props: Props) => {
                 </TooltipContent>
               </Tooltip>
             </ul>
-          ))} */}
-        {/* </TooltipProvider> */}
+          ))}
+        </TooltipProvider>
         <Separator />
         <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
           <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
@@ -76,8 +76,8 @@ const MenuOptions = (props: Props) => {
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-center flex-col gap-8">
-        {/* <ModeToggle /> */}
+      <div className="flex items-center justify-center flex-col gap-8 bg-red-500">
+        <ModeToggle />
       </div>
     </nav>
   );
